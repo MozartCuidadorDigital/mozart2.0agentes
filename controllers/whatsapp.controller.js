aa83d32c301d4ff5627836599e899a4105aa8d78
briansrt <brianstevenriofriotrochez@hotmail.com>
2025-10-14 16:06:12 -0500

    nuevos endpoints para verificacion y agendamiento

--- controllers/whatsapp.controller.js
+++ controllers/whatsapp.controller.js
@@ -67,3 +67,128 @@ export const enviarPresentacion = async (req, res) => {
   }
 };
 
+export const enviarVerificacion = async (req, res) => {
+  try {
+    const { tenant, paciente } = req.body;
+
+    if (!tenant || !paciente) {
+      return res.status(400).json({ error: "Faltan datos requeridos: tenant o paciente" });
+    }
+
+    const data = {
+      messaging_product: "whatsapp",
+      to: paciente,
+      type: "template",
+      template: {
+        name: "mozart2_verificacion",
+        language: { code: "en" },
+        components: [
+          {
+            type: "header",
+            parameters: [
+              {
+                type: "image",
+                image: {
+                  link: "https://mozartimages.s3.us-east-1.amazonaws.com/Logo_Mozart_color.png",
+                },
+              },
+            ],
+          },
+          {
+            type: "body",
+            parameters: [
+              { type: "text", text: `*IPS ${tenant.toUpperCase()}*` },
+            ],
+          },
+        ],
+      },
+    };
+
+    const response = await axios.post(
+      process.env.META_GRAPH_URL,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(200).json({
+      message: "Mensaje enviado correctamente ✅",
+      to: paciente,
+      tenant,
+      metaResponse: response.data,
+    });
+  } catch (error) {
+    console.error("❌ Error al enviar mensaje:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "No se pudo enviar el mensaje",
+      details: error.response?.data || error.message,
+    });
+  }
+};
+
+export const enviarAgendamiento = async (req, res) => {
+  try {
+    const { tenant, paciente } = req.body;
+
+    if (!tenant || !paciente) {
+      return res.status(400).json({ error: "Faltan datos requeridos: tenant o paciente" });
+    }
+
+    const data = {
+      messaging_product: "whatsapp",
+      to: paciente,
+      type: "template",
+      template: {
+        name: "mozart2_agendamiento",
+        language: { code: "en" },
+        components: [
+          {
+            type: "header",
+            parameters: [
+              {
+                type: "image",
+                image: {
+                  link: "https://mozartimages.s3.us-east-1.amazonaws.com/Logo_Mozart_color.png",
+                },
+              },
+            ],
+          },
+          {
+            type: "body",
+            parameters: [
+              { type: "text", text: `*IPS ${tenant.toUpperCase()}*` },
+            ],
+          },
+        ],
+      },
+    };
+
+    const response = await axios.post(
+      process.env.META_GRAPH_URL,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.META_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(200).json({
+      message: "Mensaje enviado correctamente ✅",
+      to: paciente,
+      tenant,
+      metaResponse: response.data,
+    });
+  } catch (error) {
+    console.error("❌ Error al enviar mensaje:", error.response?.data || error.message);
+    res.status(500).json({
+      error: "No se pudo enviar el mensaje",
+      details: error.response?.data || error.message,
+    });
+  }
+};
--- routes/whatsapp.routes.js
+++ routes/whatsapp.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { enviarPresentacion } from "../controllers/whatsapp.controller.js";
+import { enviarAgendamiento, enviarPresentacion, enviarVerificacion } from "../controllers/whatsapp.controller.js";
 
 const router = express.Router();
 
 router.post("/presentacion", enviarPresentacion);
+router.post("/verificacion", enviarVerificacion);
+router.post("/agendamiento", enviarAgendamiento);
+// router.post("/agendamiento", enviarAgendamiento);
 
 export default router;
